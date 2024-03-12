9764c7e42fe52a411312ea2cf03205aeb998b0e4
knowist <14030848+one-hun131@user.noreply.gitee.com>
2024-03-12 20:31:04 +0800

    更新开源版本至v0.0.2 ，优化图标和功能

---
+++ index.js
@@ -0,0 +1,9 @@
+function onClick() {
+  window.open('https://www.yuque.com/mepuzzle/codeio/', '_blank'); // 产品文档链接
+}
+function ai() {
+  window.open('https://aicode.knowist.cn', '_blank'); // Ai工具链接
+}
+function github() {
+  window.open('https://github.com/CnKnowist/codeio-editor', '_blank'); // Github链接
+}
